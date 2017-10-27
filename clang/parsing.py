f917bdc651d6a638ebd5f98b4288dcb367eecbc4
philipp <tdhd@users.noreply.github.com>
2017-10-27 23:16:32 +0200

    add more functions to test file and demo for computing pdists

--- clang/parsing.py
+++ clang/parsing.py
@@ -1,5 +1,7 @@
+import fnmatch
+import os
+
 import clang.cindex
-import os, fnmatch
 import pandas as pd
 import scipy.spatial.distance
 import zss
@@ -37,19 +39,46 @@ class SourceCodeParser(object):
 
         return all_functions
 
+
 if __name__ == "__main__":
     def edit_dist(first, second):
         print first
         print second
         return 0
 
+
     def zss_edit_dist(first, second):
-        # TODO
-        return 1
+        def get_children(node):
+            return list(node.get_children())
+
+        def uniform_cost(node):
+            return 1
+
+        def update_cost(a, b):
+            return 1
+
+        return zss.distance(
+            first[0], second[0],
+            get_children=get_children,
+            insert_cost=uniform_cost,
+            remove_cost=uniform_cost,
+            update_cost=update_cost
+        )
+
+
+    def normalized_zss_edit_dist(first, second):
+        d = zss_edit_dist(first, second)
+        return 2.0 * d / (len(list(first[0].walk_preorder())) + len(list(second[0].walk_preorder()))) - 1.0
+
 
     scp = SourceCodeParser('.', '*.c')
     all_functions = scp.read_functions()
 
     all_functions = pd.DataFrame({'function': all_functions}).function.values.reshape(-1, 1)
-    similarity_mat = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(all_functions, edit_dist))
+    similarity_mat = scipy.spatial.distance.squareform(
+        scipy.spatial.distance.pdist(
+            all_functions,
+            normalized_zss_edit_dist
+        )
+    )
     print similarity_mat
--- clang/test.c
+++ clang/test.c
@@ -1,7 +1,150 @@
 #include "test.h"
 
-int main(int *some_args) {
-  int x = call_me() + 1;
-  return x + some_args[0];
+
+void fsstack_copy_inode_size(struct inode *dst, struct inode *src)
+{
+        loff_t i_size;
+        blkcnt_t i_blocks;
+
+        /*
+         * i_size_read() includes its own seqlocking and protection from
+         * preemption (see include/linux/fs.h): we need nothing extra for
+         * that here, and prefer to avoid nesting locks than attempt to keep
+         * i_size and i_blocks in sync together.
+         */
+        i_size = i_size_read(src);
+
+        /*
+         * But if CONFIG_LBDAF (on 32-bit), we ought to make an effort to
+         * keep the two halves of i_blocks in sync despite SMP or PREEMPT -
+         * though stat's generic_fillattr() doesn't bother, and we won't be
+         * applying quotas (where i_blocks does become important) at the
+         * upper level.
+         *
+         * We don't actually know what locking is used at the lower level;
+         * but if it's a filesystem that supports quotas, it will be using
+         * i_lock as in inode_add_bytes().
+         */
+        if (sizeof(i_blocks) > sizeof(long))
+                spin_lock(&src->i_lock);
+        i_blocks = src->i_blocks;
+        if (sizeof(i_blocks) > sizeof(long))
+                spin_unlock(&src->i_lock);
+
+        /*
+         * If CONFIG_SMP or CONFIG_PREEMPT on 32-bit, it's vital for
+         * fsstack_copy_inode_size() to hold some lock around
+         * i_size_write(), otherwise i_size_read() may spin forever (see
+         * include/linux/fs.h).  We don't necessarily hold i_mutex when this
+         * is called, so take i_lock for that case.
+         *
+         * And if CONFIG_LBDAF (on 32-bit), continue our effort to keep the
+         * two halves of i_blocks in sync despite SMP or PREEMPT: use i_lock
+         * for that case too, and do both at once by combining the tests.
+         *
+         * There is none of this locking overhead in the 64-bit case.
+         */
+        if (sizeof(i_size) > sizeof(long) || sizeof(i_blocks) > sizeof(long))
+                spin_lock(&dst->i_lock);
+        i_size_write(dst, i_size);
+        dst->i_blocks = i_blocks;
+        if (sizeof(i_size) > sizeof(long) || sizeof(i_blocks) > sizeof(long))
+                spin_unlock(&dst->i_lock);
 }
 
+
+void deactivate_locked_super(struct super_block *s)
+{
+        struct file_system_type *fs = s->s_type;
+        if (atomic_dec_and_test(&s->s_active)) {
+                cleancache_invalidate_fs(s);
+                unregister_shrinker(&s->s_shrink);
+                fs->kill_sb(s);
+
+                /*
+                 * Since list_lru_destroy() may sleep, we cannot call it from
+                 * put_super(), where we hold the sb_lock. Therefore we destroy
+                 * the lru lists right now.
+                 */
+                list_lru_destroy(&s->s_dentry_lru);
+                list_lru_destroy(&s->s_inode_lru);
+
+                put_filesystem(fs);
+                put_super(s);
+        } else {
+                up_write(&s->s_umount);
+        }
+}
+
+int thaw_super(struct super_block *sb)
+{
+        int error;
+
+        down_write(&sb->s_umount);
+        if (sb->s_writers.frozen != SB_FREEZE_COMPLETE) {
+                up_write(&sb->s_umount);
+                return -EINVAL;
+        }
+
+        if (sb_rdonly(sb)) {
+                sb->s_writers.frozen = SB_UNFROZEN;
+                goto out;
+        }
+
+        lockdep_sb_freeze_acquire(sb);
+
+        if (sb->s_op->unfreeze_fs) {
+                error = sb->s_op->unfreeze_fs(sb);
+                if (error) {
+                        printk(KERN_ERR
+                                "VFS:Filesystem thaw failed\n");
+                        lockdep_sb_freeze_release(sb);
+                        up_write(&sb->s_umount);
+                        return error;
+                }
+        }
+
+        sb->s_writers.frozen = SB_UNFROZEN;
+        sb_freeze_unlock(sb);
+out:
+        wake_up(&sb->s_writers.wait_unfrozen);
+        deactivate_locked_super(sb);
+        return 0;
+}
+
+
+int thaw_super_(struct super_block *sb)
+{
+        int error;
+
+        down_write(&sb->s_umount);
+        if (sb->s_writers.frozen != SB_FREEZE_COMPLETE) {
+                up_write(&sb->s_umount);
+                return -EINVAL;
+        }
+
+        if (sb_rdonly(sb)) {
+                sb->s_writers.frozen = SB_UNFROZEN;
+                goto out;
+        }
+
+        lockdep_sb_freeze_acquire(sb);
+
+        if (sb->s_op->unfreeze_fs) {
+                error = sb->s_op->unfreeze_fs(sb);
+                if (error) {
+                        printk(KERN_ERR
+                                "VFS:Filesystem thaw failed\n");
+                        lockdep_sb_freeze_release(sb);
+                        up_write(&sb->s_umount);
+                        return error;
+                }
+        }
+
+        sb->s_writers.frozen = SB_UNFROZEN;
+        sb_freeze_unlock(sb);
+out:
+        wake_up(&sb->s_writers.wait_unfrozen);
+        deactivate_locked_super(sb);
+        return 0;
+}
